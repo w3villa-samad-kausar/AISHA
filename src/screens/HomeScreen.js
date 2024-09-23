e3a07525eb8ba22757ec49f689c7f26cc7b890eb
Samad Kausar <samad.kausar@w3villa.com>
2024-09-23 10:36:51 +0530

    added background

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,29 @@
-import {View} from 'react-native';
+import {ImageBackground, StyleSheet, View} from 'react-native';
 import React from 'react';
 import ButtonComponent from '../components/ButtonComponent';
 
 const HomeScreen = () => {
-  
   return (
-    <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
-      <ButtonComponent label="Image Generation" screenName='Image Generator' />
-      <ButtonComponent label="Chat Bot" screenName="Chat Bot" />
-    </View>
+    <ImageBackground
+      ImageBackground
+      source={require('../assets/images/background.jpg')}
+      style={styles.background}>
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <ButtonComponent
+          label="Image Generation"
+          screenName="Image Generator"
+        />
+        <ButtonComponent label="Chat Bot" screenName="Chat Bot" />
+      </View>
+    </ImageBackground>
   );
 };
 
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover', // or 'contain'
+    justifyContent: 'center',
+  },
+});
 export default HomeScreen;
